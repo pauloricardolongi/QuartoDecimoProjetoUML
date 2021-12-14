779e6a8333d5871df3af7b78270148ceebeea440
pauloricardolongi <pauloricardolongi@gmail.com>
2021-12-14 03:47:20 -0300

    Solução normal sem disparo exceções

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,36 @@
+package aplicacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entidades.Conta;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Informe os dados da conta");
+		System.out.print("Número: ");
+		int numero = sc.nextInt();
+		sc.nextLine();
+		System.out.print("Titular: ");
+		String titular = sc.nextLine();
+		System.out.print("Saldo Inicial: ");
+		double saldo = sc.nextDouble();
+		System.out.print("Limite de Saque: ");
+		double limiteSaque = sc.nextDouble();
+		
+		Conta acc = new Conta(numero, titular, saldo, limiteSaque);
+		System.out.println();
+		System.out.print("Informe o valor do saque");
+		double quantia = sc.nextDouble();
+		acc.saque(quantia);
+		System.out.printf("Novo Saldo: %.2f%n", acc.getSaldo());
+		sc.close();
+		
+
+	}
+
+}
---
+++ src/entidades/Conta.java
@@ -0,0 +1,60 @@
+package entidades;
+
+public class Conta {
+	private Integer numero;
+	private String nome;
+	private Double saldo;
+	private Double limiteSaque;
+	
+	public Conta() {
+		
+	}
+
+	public Conta(Integer numero, String nome, Double saldo, Double limiteSaque) {
+		
+		this.numero = numero;
+		this.nome = nome;
+		this.saldo = saldo;
+		this.limiteSaque = limiteSaque;
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(Double saldo) {
+		this.saldo = saldo;
+	}
+
+	public Double getLimiteSaque() {
+		return limiteSaque;
+	}
+
+	public void setLimiteSaque(Double limiteSaque) {
+		this.limiteSaque = limiteSaque;
+	}
+	public void depositar(double quantia) {
+		saldo = saldo + quantia;
+	}
+
+	public void saque(double quantia) {
+		saldo= saldo - quantia;
+	}
+	
+}
